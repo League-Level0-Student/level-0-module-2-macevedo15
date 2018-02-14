3e85a04268833ccec03ab904a77f1dc954ff061c
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-02-13 19:22:52 -0800

    animalfarm, moles and holes

--- src/methods/AnimalFarm.java
+++ src/methods/AnimalFarm.java
@@ -10,17 +10,36 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JOptionPane;
+import javax.swing.text.StyledEditorKit.ForegroundAction;
 
 public class AnimalFarm {
 
 AnimalFarm() {
+
 	/*
 	 * 1. Ask the user which animal they want, then play the sound of that
 	 * animal.
-	 */
+	 
+	 
 
 	/* 2. Make it so that the user can keep entering new animals. */
+//for (int i = 0; i < 10; i++) {
+//	
+//}
+	String animal=JOptionPane.showInputDialog(null, "What animal do you want?");
+
+if(animal.equals("cow")) {
+playMoo();
 }
+else if(animal.equals("duck")) {
+	playQuack();
+}
+else if(animal.equals("dog"));
+playWoof();
+}
+
+
+
 
 void playMoo() {
 	playNoise(mooFile);
@@ -45,6 +64,7 @@ String llamaFile = "llama.wav";
 
 public void playNoise(String soundFile) {
 	try {
+		 System.out.println(soundFile);
 		 Clip clip = AudioSystem.getClip();
 		 clip.open(AudioSystem.getAudioInputStream(getClass().getResource(soundFile)));
 		 clip.start();
