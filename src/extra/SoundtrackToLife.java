fa4355678c8d1e5dfc4df2ef3f9d496c7d52ef2c
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-03-06 19:18:42 -0800

    module two

--- src/extra/SoundtrackToLife.java
+++ src/extra/SoundtrackToLife.java
@@ -11,26 +11,33 @@ public class SoundtrackToLife {
 	public static void main(String[] args) {
 
 		// 1. Adjust this pop-up to find out what mood the user is in.
-		int userMood = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Mood1", "Mood2", "Mood3" }, null);
+		int userMood = JOptionPane.showOptionDialog(null, "what mood are you in?", "moodsss", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "mad", "stressed", "happy" }, null);
 		
 		// 2. Their answer is stored in the userMood variable. Print it out.
+System.out.println(userMood);
 
 		// 3. If they are in a stressed mood, use the playVideo method to play a calming song from YouTube.
+		//4. Play different songs for other moods.
+if(userMood==0){
+	playVideo("https://www.youtube.com/watch?v=de2TdvDaS5A");
+		}
+else if(userMood==1){
+	playVideo("https://www.youtube.com/watch?v=lTXVEE0jyo0");
+}
 		
-
-		
-		// 4. Play different songs for other moods.
-
+else if(userMood==2) {
+	playVideo("https://www.youtube.com/watch?v=7gPJyOtrnWY");
+}
+			
 /**
 * If you can’t think of any, here are some you can use...
 *	http://bit.ly/video-for-happy
 *	http://bit.ly/video-for-sad
 *	http://bit.ly/video-for-angry
-**/
 		
 		// If you are seeing ads at the beginning of your videos, install Adblock.
-
+*/
 	}
 
 	static void playVideo(String youTubeLink) {
